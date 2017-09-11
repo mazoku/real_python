3d28f751e31924d1a23e1b3f413e10509514c676
Tomas Ryba <rybato@gmail.com>
2017-09-11 10:00:09 +0200

    status codes and debug mode

--- realpython/book2/flask-hello-world/app.py
+++ realpython/book2/flask-hello-world/app.py
@@ -6,13 +6,28 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config['DEBUG'] = True
+
 # use the decorator pattern to link the view function to a url
 @app.route('/')
 @app.route('/hello')
-
 # defin the view using a function, which returns a string
 def hello_world():
     return 'Hello, World!'
+
+# dynamic route
+@app.route('/test/<search_query>')
+def search(search_query):
+    return search_query
+
+# dynamic route with explicit status codes
+@app.route('/name/<name>')
+def index(name):
+    if name.lower() == 'michael':
+        return 'Hello, {}'.format(name), 200
+    else:
+        return 'Not Found', 404
 
 # start the developement server using the run() method
 if __name__ == '__main__':
