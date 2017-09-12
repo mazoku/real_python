1f2d7249721ae5bd8e59aa628abffa0c3c38233c
Tomas Ryba <rybato@gmail.com>
2017-09-12 12:38:45 +0200

    FlaskTaskr_02 hotovy.

--- realpython/book2/flasktaskr/project/_config.py
+++ realpython/book2/flasktaskr/project/_config.py
@@ -4,11 +4,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 # better use hdcp-genkey
 SECRET_KEY = '=\x88\x07\xdd\xfa{\xe39\xdb\xffq\xdb\xcb0\xfd\x9b\x16\xf7EOm\x96;1'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
--- realpython/book2/flasktaskr/project/db_create.py
+++ realpython/book2/flasktaskr/project/db_create.py
@@ -1,18 +1,15 @@
-import sqlite3
-from _config import DATABASE_PATH
 
-with sqlite3.connect(DATABASE_PATH) as connection:
+from views import db
+from models import Task
+from datetime import date
 
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
 
-    # create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                 name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-                 status INTEGER NOT NULL)""")
+# create the database and the db table
+db.create_all()
 
-    # insert dummy data into table
-    c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-              'VALUES("Finish this tutorial", "12/09/2017", 10, 1)')
-    c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-              'VALUES("Finish Real Python Course 2", "17/09/2017", 10, 1)')
+# insert data
+db.session.add(Task('Finish this tutorial', date(2017, 9, 12), 10, 1))
+db.session.add(Task('Finish Real Python', date(2017, 9, 17), 9, 1))
+
+# commit the changes
+db.session.commit()
